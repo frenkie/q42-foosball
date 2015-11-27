3b171e543bcde547f64971f84c234abda0cb8840
Timo Klok <mail@timoklok.nl>
2015-11-27 15:59:46 +0100

    team colors own the table

--- tv-client/js/tv-clent.js
+++ tv-client/js/tv-clent.js
@@ -1,3 +1,7 @@
+//function fussballViz(){
+//
+//}
+
 var backgroundLayer = project.activeLayer;
 var fieldLayer = new Layer();
 var hitLayer = new Layer();
@@ -11,6 +15,14 @@ var background;
 var game;
 //var goals;
 
+var teamColors = ["red", "blue"];
+var ownerColor = "white";
+
+var replayBalls = [];
+
+var replaySeconds = 3;
+var replayPositions = [];
+
 var size = view.size;
 
 var screenSize = {
@@ -19,26 +31,45 @@ var screenSize = {
 };
 
 var mousePos;
+var useMousePos = false;
 
 var hits = [];
 
-var socket = io("http://10.42.38.110:9090");
+var socket = io( "http://10.42.38.110:9090" );
 var currentPos;
 
-socket.on('ball-positions', function ( positions ) {
-    console.log( positions.shift() );
-    currentPos = positions.shift();
-});
+createjs.Sound.registerSound( "sounds/explosion-long.wav", "explosion-long" );
+createjs.Sound.registerSound( "sounds/explosion-short.wav", "explosion-short" );
+createjs.Sound.registerSound( "sounds/goal1.wav", "goal" );
 
-socket.on('score-left', function ( event ) {
-    console.log( event );
-    game.goalScored( 0 );
-});
 
-socket.on('score-right', function ( event ) {
-    console.log( event );
-    game.goalScored( 1 );
-});
+function handleSocketEvents () {
+
+    //socket.on('reset-game', function ( positions ) {
+    background = new Background();
+    pitch = new Pitch();
+    ball = new Ball();
+    game = new GameGraphics();
+    //});
+
+
+    socket.on( 'ball-positions', function ( positions ) {
+        if ( ! useMousePos ) {
+            currentPos = positions.shift();
+        }
+    } );
+
+    socket.on( 'score-left', function ( event ) {
+        console.log( event );
+        game.goalScored( 0 );
+    } );
+
+    socket.on( 'score-right', function ( event ) {
+        console.log( event );
+        game.goalScored( 1 );
+    } );
+
+}
 
 
 var Hit = function ( posX, posY ) {
@@ -52,7 +83,7 @@ var Hit = function ( posX, posY ) {
         center : [posX, posY],
         size : [this.radius, this.radius],
         fillColor : 'rgba(0,0,0,0)',
-        strokeColor : 'red',
+        strokeColor : ownerColor,
         strokeWidth : 10
     } );
 
@@ -71,10 +102,50 @@ Hit.prototype.iterate = function () {
 };
 
 
+function ReplayBall () {
+
+    if ( ball.item ) {
+        ball.item.remove();
+    }
+    ball.removeStills();
+
+    ball = undefined;
+
+    this.radius = 50;
+
+    this.item = new Shape.Ellipse( {
+        center : [0, 0],
+        size : [this.radius, this.radius],
+        fillColor : ownerColor
+    } );
+
+
+}
+
+ReplayBall.prototype.iterate = function () {
+
+    if ( replayPositions.length > 0 ) {
+        var pos = replayPositions.shift();
+        this.item.position.x = pos[0];
+        this.item.position.y = pos[1];
+    } else {
+        this.item.remove();
+        var i = replayBalls.indexOf( this );
+        replayBalls.splice( i, 1 );
+        ball = new Ball();
+    }
+
+};
+
 var trailBalls = [];
 var TrailBall = function ( posX, posY, size ) {
 
+    var maxSize = 20;
     trailBallLayer.activate();
+
+    if ( size > maxSize ) {
+        size = maxSize;
+    }
 
     this.radius = size * 5;
     this.lifespan = 120;
@@ -85,11 +156,10 @@ var TrailBall = function ( posX, posY, size ) {
     } );
 
     this.item.fillColor = {
-        stops : ['rgba(255,255,0,0.5)', 'rgba(255,255,0,0.5)', 'rgba(255,0,0,0.5)', 'rgba(255,0,0,0)'],
+        stops : ['rgba(0,255,200,0.5)', 'rgba(0,255,255,0.5)', 'rgba(0,0,255,0.5)', 'rgba(0,0,255,0)'],
         radial : true,
         origin : [posX, posY],
-        destination : this.item.bounds.rightCenter,
-
+        destination : this.item.bounds.rightCenter
     }
 
 };
@@ -108,7 +178,6 @@ TrailBall.prototype.iterate = function () {
     this.lifespan --;
 };
 
-
 var Ball = function () {
 
     ballLayer.activate();
@@ -124,15 +193,40 @@ var Ball = function () {
     this.destination = Point.random() * (view.size + 100);
     this.stillCounter = 0;
 
-
+    this.replayPositions = [];
+    this.maxReplayPositions = replaySeconds * 60;
 };
 
-Ball.prototype.iterate = function ( position  ) {
 
-    //console.log(position);
+Ball.prototype.removeStills = function () {
+    if ( this.still ) {
+        this.still.remove();
+    }
+    if ( this.still2 ) {
+        this.still2.remove();
+    }
 
-    this.item.position.x = position.x / screenSize.x * size.width;
-    this.item.position.y = position.y / screenSize.y * size.height;
+    this.stillCounter = 0;
+    this.still2 = undefined;
+    this.still = undefined;
+};
+
+Ball.prototype.iterate = function ( position ) {
+
+    console.log( position );
+    if ( ! useMousePos ) {
+        this.item.position.x = position.x / screenSize.x * size.width;
+        this.item.position.y = position.y / screenSize.y * size.height;
+    } else {
+        this.item.position.x = position.x;
+        this.item.position.y = position.y;
+    }
+
+    replayPositions.push( [this.item.position.x, this.item.position.y] );
+
+    if ( replayPositions.length >= this.maxReplayPositions ) {
+        replayPositions.shift();
+    }
 
     if ( 0 > (this.item.position.x - this.radius / 2) ) {
         if ( this.allowNewHit ) {
@@ -162,15 +256,7 @@ Ball.prototype.iterate = function ( position  ) {
 
     if ( speed.length > 2 ) {
 
-        if ( this.still ) {
-            this.still.remove();
-            this.still = undefined;
-            this.stillCounter = 0;
-
-            if (this.still2){
-                this.still2.remove();
-            }
-        }
+        this.removeStills();
 
         var newPosX = Math.floor( Math.random() * ((this.item.pre.x - this.item.position.x) + 1) + this.item.position.x );
         var newPosY = Math.floor( Math.random() * ((this.item.pre.y - this.item.position.y) + 1) + this.item.position.y );
@@ -203,7 +289,7 @@ Ball.prototype.iterate = function ( position  ) {
                     center : [this.item.position.x, this.item.position.y],
                     size : [50, 50],
                     fillColor : 'rgba(0,0,0,0)',
-                    strokeColor : 'blue',
+                    strokeColor : ownerColor,
                     strokeWidth : 5
                 } );
             }
@@ -214,7 +300,7 @@ Ball.prototype.iterate = function ( position  ) {
                 center : [this.item.position.x, this.item.position.y],
                 size : [50, 50],
                 fillColor : 'rgba(0,0,0,0)',
-                strokeColor : 'blue',
+                strokeColor : ownerColor,
                 strokeWidth : 5
             } );
         }
@@ -238,58 +324,71 @@ Ball.prototype.iterate = function ( position  ) {
 
     //this.item.position = mousePos;
 
-
 };
 
-
-var Background = function () {
-
-    this.backgroundPrefix = '#background-';
-
-    $( this.backgroundPrefix + 1 ).addClass( 'show' );
+var Pitch = function () {
 
     fieldLayer.activate();
 
     this.fieldLines = new Group();
 
-
-    this.centerLine = new Path({
-        strokeColor : 'white',
+    this.centerLine = new Path( {
+        strokeColor : "white",
         strokeWidth : 5
-    });
+    } );
 
     this.centerLine.add( new Point( size.width / 2, 0 ) );
     this.centerLine.add( new Point( size.width / 2, size.height ) );
     this.fieldLines.addChild( this.centerLine );
 
     this.centerCircle = new Shape.Ellipse( {
-        center : [size.width/ 2 , size.height/2],
-        size : [ size.width/4, size.width/4 ],
+        center : [size.width / 2, size.height / 2],
+        size : [size.width / 4, size.width / 4],
         strokeColor : 'white',
         strokeWidth : 5
     } );
 
     this.fieldLines.addChild( this.centerCircle );
 
-    this.goalLeft  =  new Shape.Rectangle({
-        center : [0, size.height/2],
-        size : [size.height/3, size.height/3],
-        strokeColor: 'white',
-        strokeWidth: 5
+    this.goalLeft = new Shape.Rectangle( {
+        center : [0, size.height / 2],
+        size : [size.height / 3, size.height / 3],
+        strokeColor : 'white',
+        strokeWidth : 5
 
-    });
+    } );
     this.fieldLines.addChild( this.goalLeft );
 
-    this.goalRight  =  new Shape.Rectangle({
-        center : [size.width, size.height/2],
-        size : [size.height/3, size.height/3],
-        strokeColor: 'white',
-        strokeWidth: 5
-    });
+    this.goalRight = new Shape.Rectangle( {
+        center : [size.width, size.height / 2],
+        size : [size.height / 3, size.height / 3],
+        strokeColor : 'white',
+        strokeWidth : 5
+    } );
 
     this.fieldLines.addChild( this.goalRight );
+};
+
+Pitch.prototype.change = function ( color ) {
+
+    this.fieldLines.strokeColor = color;
+
+};
+
+var Background = function () {
+
+    this.backgroundPrefix = '#background-';
+
+    backgroundLayer.activate();
+
+    this.backdrop = new Shape.Rectangle( {
+        center : [size.width / 2, size.height / 2],
+        size : [size.width, size.height],
+        fillColor : 'black'
+    } );
 
 
+    this.currentBackground = 1;
 
 
 };
@@ -297,71 +396,54 @@ var Background = function () {
 Background.prototype.change = function ( index ) {
 
     $( '.show' ).removeClass( 'show' );
-    backgroundLayer.activate();
+    $( this.backgroundPrefix + index ).addClass( 'show' );
 
-    if ( index == 0 ) {
-        this.fieldLines.strokeColor = 'white';
-        if (this.backdrop){
-            this.backdrop.remove();
-        }
-        $( this.backgroundPrefix + index ).addClass( 'show' );
-
-    } else if ( index == 1 ) {
-        this.fieldLines.strokeColor = 'white';
-
-        if (this.backdrop){
-            this.backdrop.remove();
-        }
-        $( this.backgroundPrefix + index ).addClass( 'show' );
-
-    }else if ( index == 2 ) {
-
-        this.fieldLines.strokeColor = 'blue';
-
-        this.backdrop  =  new Shape.Rectangle({
-            center : [size.width/2, size.height/2],
-            size : [size.width, size.height],
-            fillColor: 'black'
-        });
+    if ( this.backdrop ) {
+        this.backdrop.remove();
     }
+
 };
 
-function Goal(team) {
+function Goal ( team ) {
 
-    this.lifespan = 60;
+    pitch.change( teamColors[team] );
+
+    this.lifespan = 0;
 
     overlayLayer.activate();
 
-    if ( team == 0 ) {
-        this.item = new Shape.Ellipse( {
-            center : [0, size.height / 2],
-            size : [200, 200],
-            fillColor : 'red'
-        } );
-    } else if ( team == 1 ) {
-        this.item = new Shape.Ellipse( {
-            center : [size.width, size.height / 2],
-            size : [200, 200],
-            fillColor : 'red'
-        } );
-    }
+    this.scoreText = new PointText( {
+        point : view.center,
+        justification : 'center',
+        fontSize : 20,
+        fillColor : 'white'
+    } );
+
+    this.scoreText.content = "GOAL!!";
+    this.scoreText.rotation = 0;
+
+    createjs.Sound.play( 'goal' );
 
 }
 
-Goal.prototype.iterate = function() {
+Goal.prototype.iterate = function () {
 
-        if ( this.lifespan == 0 ) {
-            this.item.remove();
-            var i = game.goals.indexOf( this );
-            game.goals.splice( i, 1 );
-        }
-        this.lifespan --;
+    this.scoreText.scale( this.lifespan.map( 0, 180, 1, 1.3 ) );
+    this.scoreText.rotation = this.scoreText.rotation + 8;
+
+
+    if ( this.lifespan == 180 ) {
+        this.scoreText.remove();
+        var i = game.goals.indexOf( this );
+        game.goals.splice( i, 1 );
+    }
+    this.lifespan ++;
 };
 
 
 function GameGraphics () {
     this.score = [0, 0];
-    this.goals =[];
+    this.goals = [];
 
     scoreLayer.activate();
 
@@ -384,7 +466,9 @@ GameGraphics.prototype = {
 
         this.goals.push( new Goal( team ) );
 
-        //this.updateScore();
+        replayBalls.push( new ReplayBall() );
+
+        //background.change();
     },
 
     updateScore : function () {
@@ -396,16 +480,9 @@ GameGraphics.prototype = {
 };
 
 
-
-
 // Dummy events
 function onKeyUp ( event ) {
-    //if ( event.key == '1' ) {
-    //    game.goalScored( 0 );
-    //}
-    //if ( event.key == '2' ) {
-    //    game.goalScored( 1 );
-    //}
+
     if ( event.key == '1' ) {
         background.change( 0 );
     }
@@ -415,52 +492,60 @@ function onKeyUp ( event ) {
     if ( event.key == '3' ) {
         background.change( 2 );
     }
+    if ( event.key == '4' ) {
+        background.change( 3 );
+    }
+
+    if ( event.key == '5' ) {
+        game.goalScored( 0 );
+    }
+    if ( event.key == '6' ) {
+        game.goalScored( 1 );
+    }
+
+    if ( event.key == '0' ) {
+        useMousePos = ! useMousePos;
+        //console.log( useMousePos );
+    }
 }
 
 
 function onFrame ( event ) {
 
-    for ( var i = 0; i < hits.length; i ++ ) {
-        hits[i].iterate();
+    if ( game ) {
+        for ( var i = 0; i < hits.length; i ++ ) {
+            hits[i].iterate();
+        }
+
+        for ( var y = 0; y < trailBalls.length; y ++ ) {
+            trailBalls[y].iterate();
+        }
+
+
+        for ( var g = 0; g < game.goals.length; g ++ ) {
+            game.goals[g].iterate();
+        }
+
+        if ( replayBalls.length ) {
+            replayBalls[0].iterate();
+        }
+
+        if ( ball && currentPos ) {
+            ball.iterate( currentPos );
+        }
+
     }
-
-    for ( var y = 0; y < trailBalls.length; y ++ ) {
-        trailBalls[y].iterate();
-    }
-
-    for ( var g = 0; g < game.goals.length; g ++ ) {
-        game.goals[g].iterate();
-    }
-
-
-    if (ball && currentPos ){
-        ball.iterate( currentPos );
-    }
-
 }
 
 
 function onMouseMove ( event ) {
-    mousePos = event.point;
+    if ( useMousePos )
+        currentPos = event.point;
 }
 
-// UTILS
-//function lineDistance ( point1x, point1y, point2x, point2y ) {
-//    var xs = 0;
-//    var ys = 0;
-//
-//    xs = point2x - point1x;
-//    xs = xs * xs;
-//
-//    ys = point2y - point1y;
-//    ys = ys * ys;
-//
-//    return Math.floor( Math.sqrt( xs + ys ) );
-//}
+Number.prototype.map = function ( in_min, in_max, out_min, out_max ) {
+    return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+};
 
 
-jQuery( document ).ready( function () {
-    background = new Background();
-    ball = new Ball();
-    game = new GameGraphics();
-});
+handleSocketEvents();
